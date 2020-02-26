ae74f6dc3dfa99ffc892bf3fbb8d8d7c8ee2f9d0
RaifePaiva <raife.tec@gmail.com>
2020-02-26 00:37:41 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,21 +1,37 @@
 package com.educandoweb.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		// o L acompanhado do ID representa o LONG tipo de dado mda variavel
-		User u = new User(1L, "Raífe Ferreira", "raife.paiva@gmail.com", "21980745821", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 }
